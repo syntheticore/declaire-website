b526691bf1ba49ee4aeac0f354160810dabce48f
Björn Breitgoff <syntheticore@gmail.com>
2015-03-06 01:47:21 +0100

    Use new relic

--- newrelic.js
+++ newrelic.js
@@ -12,7 +12,6 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key : 'license key here',
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing

79b1636ce983be7d9c4bcad86bedfc49c6e4e46e
Björn Breitgoff <syntheticore@gmail.com>
2015-03-06 01:36:03 +0100

    Use new relic

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+if(typeof(window) == undefined) {
+  require('newrelic');
+}
+
 var declaire = require('declaire');
-
-
 
 declaire.start({
   mongoDevUrl: 'mongodb://127.0.0.1:27017/declaire-website'

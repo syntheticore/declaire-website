d183fb628e819da61b7350dfb44f4fa7a6595507
Björn Breitgoff <syntheticore@gmail.com>
2015-03-11 15:28:33 +0100

    Fix css columns in FF

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var declaire = require('declaire');
 
+if(typeof(window) != 'undefined') {
+  // Migitate weird css columns behaviour in FF
+  $('.features').css('max-width', 100);
+  setTimeout(function() {
+    $('.features').css('max-width', 900);
+    setTimeout(function() {
+      $('.features').css('max-width', 950);
+    }, 100);
+  }, 100);
+}
+
 declaire.start({
   mongoDevUrl: 'mongodb://127.0.0.1:27017/declaire-website'
 });

85ff7d433015a05e8166013eba4c85980c936787
Björn Breitgoff <syntheticore@gmail.com>
2015-03-12 06:59:31 +0100

    Fix cross browser font size for code

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 var declaire = require('declaire');
 
-if(typeof(window) != 'undefined') {
-  // Migitate weird css columns behaviour in FF
-  $('.features').css('max-width', 100);
-  setTimeout(function() {
-    $('.features').css('max-width', 900);
-    setTimeout(function() {
-      $('.features').css('max-width', 950);
-    }, 100);
-  }, 100);
-}
+if(typeof(window) != 'undefined') {}
 
 declaire.start({
   mongoDevUrl: 'mongodb://127.0.0.1:27017/declaire-website'

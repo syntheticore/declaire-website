7633b731257d7c3d21a304be7437e6d8554a94dd
Björn Breitgoff <syntheticore@gmail.com>
2015-03-13 02:51:28 +0100

    Reenable new relic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+if(typeof(window) == 'undefined') {
+  require('newrelic');
+}
 var declaire = require('declaire');
-
-if(typeof(window) != 'undefined') {}
 
 declaire.start({
   mongoDevUrl: 'mongodb://127.0.0.1:27017/declaire-website'

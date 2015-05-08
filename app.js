783ef096f3df9b7045c92b6ac56b8fbe85c619a2
Björn Breitgoff <syntheticore@gmail.com>
2015-05-08 19:42:53 +0200

    Drop express dependency

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 if(typeof(window) == 'undefined') { require('newrelic') }
 
 var declaire = require('declaire');
-var Express = require('express');
 var hljs = require('highlight.js');
 
 var app = declaire.Application({
-  mongoDevUrl: 'mongodb://127.0.0.1:27017/declaire-website'
+  mongoDevUrl: 'mongodb://127.0.0.1:27017/declaire-website',
+  npmPublic: ['font-awesome', 'highlight.js']
 });
 
 app.init(function(start, express, db) {
-  if(declaire.Utils.onServer()) {
-    express.use('/fontawesome', Express.static('./node_modules/font-awesome'));
-    express.use('/highlightjs', Express.static('./node_modules/highlight.js'));
-  }
   start(function() {
     if(declaire.Utils.onClient()) {
       hljs.initHighlighting();

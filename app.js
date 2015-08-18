5e60702513510152d4c5ec99c4d8992c602d3598
Björn Breitgoff <syntheticore@gmail.com>
2015-08-18 02:56:08 +0200

    Added examples page

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 if(typeof(window) == 'undefined') { require('newrelic') }
 
 var declaire = require('declaire');
-var _ = declaire.Utils;
 var hljs = require('highlight.js');
+var _ = declaire.Utils;
 
 var app = declaire.Application({
   mongoDevUrl: 'mongodb://127.0.0.1:27017/declaire-website',
   npmPublic: ['font-awesome', 'highlight.js']
 });
 
-app.ViewModel('IndexView', {}, null, function() {
+app.ViewModel('HighlightView', {}, null, function() {
+  var self = this;
   if(_.onClient()) {
-    this.el.find("code").each(function() { 
-      hljs.highlightBlock(this);
-    });
+    _.defer(function() {
+      self.el.find("code").each(function() { 
+        hljs.highlightBlock(this);
+      });
+    }, 100);
   }
 });
 
